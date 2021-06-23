b5d409133abfce92bcb4069a19bb8b92be014d9f
kristaps <kristaps.vitolins@gmail.com>
2021-06-23 19:15:00 +0300

    connecting to web camera and get some frames

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import cv2
 
-def print_hi(name):
+def start_watching(name):
     print(f'Mila is watching you {name}')
+    capture = cv2.VideoCapture(0)
+
+    capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1000)
+    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+    if not(capture.isOpened()):
+        print("Ayes are not opening")
+    else:
+        while(True):
+            ret, frame = capture.read()
+            if ret:
+                cv2.imshow('I''m watching you', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        capture.release()
+        cv2.destroyAllWindows();
 
 if __name__ == '__main__':
-    print_hi('Car owner')
+    start_watching('Car owner')
